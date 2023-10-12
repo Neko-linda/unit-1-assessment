836060d72e28e52e8b06215ba334686541897003
User Name <emelinda.gutierrez04@gmail.com>
2023-10-11 22:05:39 -0400

    Added js objects

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 console.log('test');
+
+const countDisplay = document.getElementById('counter-container')
+const incrementButton = document.getElementsById('increment')
+const decrementButton = document.getElementById('decrement')
+const inputElement = document.getElementById('input')
+
+let count = 0;
+
+function updateCount() {
+    countDisplay.textContent = count;
+}
+
+incrementButton.addEventListener()
+
+decrementButton.addEventListener()
