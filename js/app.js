5293dbc7a952fced42c283a4b7dd34a38bcdd225
User Name <emelinda.gutierrez04@gmail.com>
2023-10-11 22:31:11 -0400

    added updateCount function and button functionality

--- js/app.js
+++ js/app.js
@@ -1,22 +1,26 @@
 console.log('test');
 
-const countDisplay = document.getElementById('counter-container')
-const incrementButton = document.getElementsById('increment')
-const decrementButton = document.getElementById('decrement')
-const inputElement = document.getElementById('input')
+const countDisplay = document.getElementById('counter-container');
+const incrementButton = document.getElementById('increment');
+const decrementButton = document.getElementById('decrement');
+const inputElement = document.getElementById('input');
 
 let count = 0;
 
 function updateCount() {
     countDisplay.textContent = count;
-}
+};
 
 incrementButton.addEventListener("click", () => {
-
-})
+    const inputVal = parseInt(inputElement.value, 10);
+    count += inputVal;
+    updateCount();
+});
 
 decrementButton.addEventListener("click", () => {
-    
-})
+    const inputVal = parseInt(inputElement.value, 10);
+    count -= inputVal;
+    updateCount();
+});
 
-// updateCount();
+updateCount();
